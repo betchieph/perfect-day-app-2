ebc45829979fe17bafe136f2381d6b8ae99fdfab
betchieph <elizabeth.voyagessouth.org>
2020-11-07 21:10:13 +0000

    perfect day finished

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <h3>Travel Destination Favorites</h3>
+    </div>
+  );
+}
+export default Navbar;
